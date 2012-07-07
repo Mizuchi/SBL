3e3940778f755c091017114c548b3d85fb1e85f0
Tianjiao Yin <ytj000@gmail.com>
2012-07-07 22:35:54 +0800

    OrderStatisticTree: does not need nullPtr anymore.

--- sbl/structure/order_statistic_tree.hpp
+++ sbl/structure/order_statistic_tree.hpp
@@ -22,34 +22,37 @@ namespace intrusive {
 template < class NodePtr >
 class OrderStatisticTree;
 
-size_t random() {
-    size_t c = rand() * rand();
-    return c ? c : 1;
-}
-
 template<class NodePtr>
 class OrderStatisticTreeNode {
         friend class OrderStatisticTree<NodePtr>;
         NodePtr child[2];
         size_t size;
         size_t priority;
+        size_t random() {
+            size_t c = rand() * rand() + 1;
+            return c;
+        }
+    public:
+        OrderStatisticTreeNode(): size(1), priority(random()) {
+            child[0] = child[1] = NULL;
+        }
 };
 
 /** @brief order statistic tree is a data structure described in Introduction
  * to Algorithm.
-*
-* This is an augmenting data structure, which add size attribute to every tree
-* nodes.  Size attribute represent how many nodes in current subtree (treat the
-* node we focus as root). For more information, read CLRS Chapter 14.
-*
-* 
-* @tparam NodePtr a pointer to the node which defined by user.
-*/
+ *
+ * This is an augmenting data structure, which add size attribute to every tree
+ * nodes.  Size attribute represent how many nodes in current subtree (treat the
+ * node we focus as root). For more information, read CLRS Chapter 14.
+ *
+ *
+ * @tparam NodePtr a pointer to the node which defined by user.
+ */
 
 template < class NodePtr >
 class OrderStatisticTree {
-    /// This data structure implement by using Treap.
-    /// Treap is a binary search tree.
+        /// This data structure implement by using Treap.
+        /// Treap is a binary search tree.
     private:
 
         /// get the node information from NodePtr
@@ -57,104 +60,204 @@ class OrderStatisticTree {
             return a->node;
         }
 
-        /// get the left child from a NodePtr
-        NodePtr &l(NodePtr a) {
-            return get_node(a).child[0];
+        /// get the node information from NodePtr, const version
+        const OrderStatisticTreeNode<NodePtr> &get_node(NodePtr a) const {
+            return a->node;
         }
 
-        /// get the right child from a NodePtr
-        NodePtr &r(NodePtr a) {
-            return get_node(a).child[1];
+        /// @return number of nodes in subtree a.
+        const size_t &size(NodePtr a) const {
+            static const size_t zero = 0;
+            if (a == NULL) return zero;
+            return get_node(a).size;
         }
 
-        /// get the child according index.
-        /// 
-        /// @param index 0 represent left child, 1 represent right child.
-        ///
-        NodePtr &child(NodePtr a, size_t idx) {
-            if (idx == 0) return l(a);
-            if (idx == 1) return r(a);
-            assert(false && "OrderStatisticTree:: subscript range error");
+        /// change size attribute.
+        void set_size(NodePtr a, size_t value) {
+            if (a == NULL) return;
+            get_node(a).size = value;
         }
 
         /// get priority from a NodePtr for treap
-        size_t &priority(NodePtr a) {
+        const size_t &priority(NodePtr a) const {
+            static const size_t zero = 0;
+            if (a == NULL) return zero;
             return get_node(a).priority;
+        }
+
+        /// get the child according index.
+        ///
+        /// @param a father
+        /// @param index 0 represent left child, 1 represent right child.
+        ///
+        /// @return const reference of child
+        const NodePtr &child(NodePtr a, size_t idx) const {
+            static const NodePtr kNullPtr = NULL;
+            if (a == NULL) return kNullPtr;
+            return get_node(a).child[idx];
+            assert(false && "OrderStatisticTree:: subscript range error");
+        }
+
+        /// get the reference of child according index.
+        ///
+        /// @param a father, must not be NULL
+        /// @param index 0 represent left child, 1 represent right child.
+        ///
+        /// @return reference of child
+        NodePtr &child_references(NodePtr a, size_t idx) {
+            assert(a != NULL);
+            return get_node(a).child[idx];
+            assert(false and "OrderStatisticTree:: subscript range error");
+        }
+
+        /// change child
+        void set_child(NodePtr a, size_t idx, NodePtr b) {
+            if (a == NULL) return;
+            get_node(a).child[idx] = b;
+        }
+
+        /// get the left child from a NodePtr
+        const NodePtr &l(NodePtr a) const {
+            return child(a, 0);
+        }
+
+        /// get the right child from a NodePtr
+        const NodePtr &r(NodePtr a) const {
+            return child(a, 1);
         }
 
         /// update the size of current NodePtr
         void update_size(NodePtr a) {
-            size(a) = size(l(a)) + size(r(a)) + 1;
+            set_size(a, size(l(a)) + size(r(a)) + 1);
         }
 
         /// Swap a NodePtr with her father.
         NodePtr rotate(NodePtr t, size_t idx) {
             assert(idx == 0 or idx == 1);
             NodePtr s = child(t, idx);
-            child(t, idx) = child(s, 1 - idx);
-            child(s, 1 - idx) = t;
+            set_child(t, idx, child(s, 1 - idx));
+            set_child(s, 1 - idx, t);
             update_size(t);
             update_size(s);
             return s;
         }
 
-        /// Add NodePtr a to subtree x.
+        /// Add NodePtr x to subtree a.
         void add(NodePtr x, NodePtr &a) {
-            if (a == nullPtr) {
+            if (a == NULL) {
                 a = x;
-                l(a) = r(a) = nullPtr;
-                size(a) = 1;
-                priority(a) = random();
             } else {
-                int c = x->compare(a) ? 0 : 1;
-                add(x, child(a, c));
-                if (priority(child(a, c)) <= priority(a))
-                    size(a)++;
+                size_t idx = x->less_than(a) ? 0 : 1;
+                add(x, child_references(a, idx));
+                if (priority(child(a, idx)) <= priority(a))
+                    update_size(a);
                 else
-                    a = rotate(a, c);
+                    a = rotate(a, idx);
             }
 
         }
 
         /// Delete NodePtr a from subtree x.
         NodePtr del(NodePtr x, NodePtr &a) {
-            if (a == nullPtr) return 0;
-            int c = x->compare(a) ? 0 : a->compare(x) ? 1 : 2;
-            if (c == 2) {
-                if (l(a) == r(a) and  r(a) == nullPtr) {
+            if (a == NULL) return 0;
+            size_t idx = x->less_than(a) ? 0 : a->less_than(x) ? 1 : 2;
+            if (idx == 2) {
+                // x and a are equivalence
+                if (l(a) == r(a) and r(a) == NULL) {
                     NodePtr ret = a;
-                    a = nullPtr;
+                    a = NULL;
                     return ret;
                 }
-                c = priority(l(a)) > priority(r(a)) ? 1 : 0;
-                a = rotate(a, 1 - c);
+                idx = priority(l(a)) > priority(r(a)) ? 1 : 0;
+                a = rotate(a, 1 - idx);
             }
-            NodePtr ret = del(x, child(a, c));
+            NodePtr ret = del(x, child_references(a, idx));
             if (ret != 0)
-                size(a)--;
+                update_size(a);
             return ret;
         }
 
-        /// @return number of nodes in subtree a.
-        size_t &size(NodePtr a) {
-            return get_node(a).size;
+        /// @return number of nodes in subtree a smaller than x.
+        size_t smaller_count(NodePtr x, NodePtr a) const {
+            if (a == NULL)
+                return 0;
+            else if (not a->less_than(x))
+                return smaller_count(x, l(a));
+            else
+                return smaller_count(x, r(a)) + size(l(a)) + 1;
         }
 
-        NodePtr root;    
-        NodePtr nullPtr; ///< a sentinel denote the empty pointer
-    public:
-        /** Since OrderStatisticTree could not be allocated memory for a NodePtr,
-         * the user must given a useless NodePtr as a sentinel.
-         */
-        OrderStatisticTree(NodePtr a): root(a), nullPtr(a) {
-            l(a) = r(a) = a;
-            size(a) = 0;
-            priority(a) = 0;
+        /// @return number of nodes in subtree a bigger than x.
+        size_t bigger_count(NodePtr x, NodePtr a) const {
+            if (a == NULL)
+                return 0;
+            else if (not x->less_than(a))
+                return bigger_count(x, r(a));
+            else
+                return bigger_count(x, l(a)) + size(r(a)) + 1;
         }
+
+        /// @return node in subtree a smaller than x.
+        NodePtr smaller(NodePtr x, NodePtr a) const {
+            if (a == NULL)
+                return 0;
+            else if (not a->less_than(x)) {
+                return smaller(x, l(a));
+            } else {
+                NodePtr b = smaller(x, r(a));
+                return b == NULL ? a : b;
+            }
+        }
+
+        /// @return node in subtree a bigger than x.
+        NodePtr bigger(NodePtr x, NodePtr a) const {
+            if (a == NULL)
+                return 0;
+            else if (not x->less_than(a)) {
+                return bigger(x, r(a));
+            } else {
+                NodePtr b = bigger(x, l(a));
+                return b == NULL ? a : b;
+            }
+        }
+
+        /// @return smallest node in subtree a
+        NodePtr smallest(NodePtr a) const {
+            if (a == NULL || l(a) == NULL) return a;
+            return smallest(l(a));
+        }
+        /// @return biggest node in subtree a
+        NodePtr biggest(NodePtr a) const {
+            if (a == NULL || r(a) == NULL) return a;
+            return biggest(r(a));
+        }
+
+        /// @return n-th small node in subtree a.
+        NodePtr at(size_t n, NodePtr a) const {
+            if (a == 0) a = root;
+            size_t ls = size(l(a));
+            return ls == n ? a :
+                   ls >  n ? at(n, l(a)) :
+                   at(n - ls - 1, r(a));
+        }
+
+        /// @return equivalence of x in subtree a.
+        NodePtr find(NodePtr x, NodePtr a) const {
+            if (a == 0) a = root;
+            return a == NULL  ? 0 :
+                   x->less_than(a) ? find(x, l(a)) :
+                   a->less_than(x) ? find(x, r(a)) : a;
+        }
+
+        NodePtr root;
+    public:
+        
+        /// construct an empty OrderStatisticTree
+        OrderStatisticTree(): root(NULL) {}
 
         /// @return number of all nodes.
-        size_t size() const{
-            return root->node.size;
+        size_t size() const {
+            return size(root);
         }
 
         /// add NodePtr x to root.
@@ -167,79 +270,42 @@ class OrderStatisticTree {
             return del(x, root);
         }
 
-        /// @param x the NodePtr for comparing.
-        /// @param a which subtree we are focused.
-        ///
-        /// @return number of nodes that (key) smaller than NodePtr x.
-        size_t smaller_count(NodePtr x, NodePtr a = 0) {
-            if (a == 0) a = root;
-            if (a == nullPtr) return 0;
-            if (!a->compare(x)) return smaller_count(x, l(a));
-            return smaller_count(x, r(a)) + size(l(a)) + 1;
+        /// @return number of nodes that smaller than NodePtr x.
+        size_t smaller_count(NodePtr x) const {
+            return smaller_count(x, root);
         }
 
-        /// @param x the NodePtr for comparing.
-        /// @param a which subtree we are focused.
-        ///
-        /// @return number of nodes that (key) bigger than NodePtr x.
-        size_t bigger_count(NodePtr x, NodePtr a = 0) {
-            if (a == 0) a = root;
-            if (a == nullPtr) return 0;
-            if (!x->compare(a)) return bigger_count(x, r(a));
-            return bigger_count(x, l(a)) + size(r(a)) + 1;
+        /// @return number of nodes that bigger than NodePtr x.
+        size_t bigger_count(NodePtr x) const {
+            return bigger_count(x, root);
         }
-        /// @param x the NodePtr for comparing.
-        /// @param a which subtree we are focused.
-        ///
         /// @return first NodePtr smaller than NodePtr x.
-        NodePtr smaller(NodePtr x, NodePtr a = 0) {
-            if (a == 0) a = root;
-            if (a == nullPtr) return 0;
-            if (!a->compare(x)) return smaller(x, l(a));
-            else {
-                NodePtr b = smaller(x, r(a));
-                return b == 0 ? a : b;
-            }
+        NodePtr smaller(NodePtr x) const {
+            return smaller(x, root);
         }
-        /// @param x the NodePtr for comparing.
-        /// @param a which subtree we are focused.
-        ///
         /// @return first NodePtr bigger than NodePtr x.
-        NodePtr bigger(NodePtr x, NodePtr a = 0) {
-            if (a == 0) a = root;
-            if (a == nullPtr) return 0;
-            if (!x->compare(a)) return bigger(x, r(a));
-            else {
-                NodePtr b = bigger(x, l(a));
-                return b == 0 ? a : b;
-            }
+        NodePtr bigger(NodePtr x) const {
+            return bigger(x, root);
         }
-        /// @return the minimum node
-        NodePtr smallest(NodePtr a = 0) {
-            if (a == 0) a = root;
-            if (a == nullPtr || l(a) == nullPtr) return a;
-            return smallest(l(a));
+        /// @return the minimum node.
+        NodePtr smallest() const {
+            return smallest(root);
         }
-        /// @return the maximum node
-        NodePtr biggest(NodePtr a = 0) {
-            if (a == 0) a = root;
-            if (a == nullPtr || r(a) == nullPtr) return a;
-            return biggest(r(a));
+        /// @return the maximum node.
+        NodePtr biggest() const {
+            return biggest(root);
         }
         /// @return the n-th small node.
-        NodePtr at(size_t n, NodePtr a = 0) {
-            if (a == 0) a = root;
-            size_t ls = size(l(a));
-            return ls == n ? a :
-                   ls >  n ? at(n, l(a)) :
-                   at(n - ls - 1, r(a));
+        NodePtr at(size_t n) const {
+            return at(n, root);
         }
-        /// @return the node equal to x.
-        NodePtr find(NodePtr x, NodePtr a = 0) {
-            if (a == 0) a = root;
-            return a == nullPtr  ? 0 :
-                   x->compare(a) ? find(x, l(a)) :
-                   a->compare(x) ? find(x, r(a)) : a;
+        /// @return equivalence node of x.
+        NodePtr find(NodePtr x) const {
+            return find(x, root);
+        }
+        /// @return remove all node in OrderStatisticTree
+        void clear() {
+            root = NULL;
         }
 };
 
