4cd6e5e105c467743c0a7bccfe2ce4c9701a2812
Tianjiao Yin <ytj000@gmail.com>
2012-07-16 21:26:05 +0800

    improve radix_sort and use lemon's radix_sort unittest

--- sbl/algorithm/sort.hpp
+++ sbl/algorithm/sort.hpp
@@ -5,45 +5,56 @@
 #include<iterator>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 #include"../utility/foreach.hpp"
-#include<iostream>
-using namespace std;
+#include"../utility/type_traits.hpp"
+
+// http://lemon.cs.elte.hu/pub/doc/latest-svn/a00854.html
 
 namespace sbl {
-template<class Iterator, size_t digits = 8>
+
+struct Unchange {
+    template<class T> T operator()(T a) const {
+        return a;
+    }
+};
+
+template<class Iterator, class Functor, size_t digits = 8>
 class RadixSort {
     private:
         template<bool v> struct Bool {
             static const bool value = v;
         };
-
-        typedef typename std::iterator_traits<Iterator>::value_type value_type;
-        static const size_t maxDigits = std::numeric_limits<value_type>::digits;
+        const Functor &functor;
         Iterator beg, end;
 
-        std::vector<value_type> data, newData;
+        typedef typename std::iterator_traits<Iterator>::value_type OriginData;
+        typedef typename result_of<Functor(OriginData)>::type value_type;
+        static const size_t maxDigits = std::numeric_limits<value_type>::digits;
+        std::vector<OriginData> data, newData;
         value_type maxElement;
+
+        template<size_t current>
+        size_t get_index(OriginData v) {
+            STATIC_ASSERT(current < maxDigits,
+                          RadixSortError_current_digit_is_too_large);
+            value_type x = functor(v);
+            x /= 1 << current;
+            x %= 1 << digits;
+            return std::abs(x);
+        }
 
         template<size_t current>
         void counting_sort(Bool<false>) {}
 
         template<size_t current>
         void counting_sort(Bool<true>) {
-
-            class {
-                public:
-                    size_t operator()(value_type v) {
-                        STATIC_ASSERT(current < maxDigits, 
-                                RadixSortError_current_digit_is_too_large);
-                        v /= 1 << current;
-                        v %= 1 << digits;
-                        return v < 0 ? -v : v;
-                    }
-            } get_index;
+            if ((maxElement >> current) == 0)
+                return;
 
             std::vector<size_t> count(1 << digits, 0);
             foreach(v, data) {
-                size_t idx = get_index(v);
+                size_t idx = get_index<current>(v);
                 assert(idx < (1 << digits));
                 count[idx] ++;
             }
@@ -52,44 +63,55 @@ class RadixSort {
                 count[idx] += count[idx - 1];
 
             rforeach(v, data) {
-                size_t idx = get_index(v);
+                size_t idx = get_index<current>(v);
                 assert(idx < (1 << digits));
                 count[idx]--;
                 assert(count[idx] < newData.size());
                 newData[count[idx]] = v;
             }
+
             data.swap(newData);
             counting_sort < current + digits > (
                 Bool < current + digits < maxDigits>());
         }
 
+        void treat_negative_number() {
+            newData.clear();
+            rforeach(v, data) {
+                if (functor(v) < 0)
+                    newData.push_back(v);
+            }
+            foreach(v, data) {
+                if (functor(v) >= 0)
+                    newData.push_back(v);
+            }
+            data.swap(newData);
+        }
+
         void sort() {
             counting_sort<0>(Bool<true>());
-            if (std::numeric_limits<value_type>::is_signed) {
-                newData.clear();
-                rforeach(v, data) {
-                    if (v < 0)
-                        newData.push_back(v);
-                }
-                foreach(v, data) {
-                    if (v >= 0)
-                        newData.push_back(v);
-                }
-                assert(data.size() == newData.size());
-                data.swap(newData);
-            }
+            treat_negative_number();
         }
 
     public:
-        RadixSort(Iterator beg, Iterator end)
-            : beg(beg), end(end),
-              data(beg, end), newData(data),
-              maxElement(*max_element(beg, end)) {
-            sort();
+        RadixSort(Iterator beg, Iterator end, Functor const &functor)
+            : functor(functor),
+              beg(beg), end(end),
+              data(beg, end), newData(data) {
+            if (beg != end) {
+                maxElement = std::abs(functor(data[0]));
+                foreach(v, data) {
+                    value_type x = functor(v);
+                    if (maxElement < std::abs(x))
+                        maxElement = std::abs(x);
+                }
+                sort();
+            }
         }
 
 
         ~RadixSort() {
+            // write back
             size_t idx = 0;
             for (Iterator iter = beg; iter != end; ++iter) {
                 *iter = data[idx];
@@ -98,13 +120,49 @@ class RadixSort {
         }
 };
 
+/// \ingroup auxalg
+///
+/// \brief Sorts the STL compatible range into ascending order.
+///
+/// The radix_sort sorts an STL compatible range into ascending
+/// order.  The radix sort algorithm can sort items which are mapped
+/// to integers with an adaptable unary function functor and the
+/// order will be ascending according to these mapped values.
+///
+/// It is also possible to use a normal function instead
+/// of the functor object. If the functor is not given it will use
+/// the identity function instead.
+///
+/// This is a special quick sort algorithm where the pivot
+/// values to split the items are choosen to be 2<sup>k</sup>
+/// for each k.
+/// Therefore, the time complexity of the algorithm is O(log(c)*n) and
+/// it uses O(n) additional space, where c is the maximal value
+/// and n is the number of the items in the container.
+///
+/// \param beg The begin of the given range.
+/// \param ned The end of the given range.
+/// \param functor An adaptible unary function or a normal function
+/// which maps the items to any integer type which can be either
+/// signed or unsigned.
+template<class BegIter, class EndIter, class Functor>
+void radix_sort(BegIter beg, EndIter end, Functor functor) {
+    RadixSort<EndIter, Functor>(beg, end, functor);
+}
+
+template<size_t digits, class BegIter, class EndIter, class Functor>
+void radix_sort_digits(BegIter beg, EndIter end, Functor functor) {
+    RadixSort<EndIter, Functor, digits>(beg, end, functor);
+}
+
 template<class BegIter, class EndIter>
 void radix_sort(BegIter beg, EndIter end) {
-    RadixSort<EndIter>(beg, end);
+    radix_sort(beg, end, Unchange());
 }
+
 template<size_t digits, class BegIter, class EndIter>
 void radix_sort_digits(BegIter beg, EndIter end) {
-    RadixSort<EndIter, digits>(beg, end);
+    radix_sort<digits>(beg, end, Unchange());
 }
 
 } // namespace sbl
--- unittest/algorithm/sort.cpp
+++ unittest/algorithm/sort.cpp
@@ -65,3 +65,126 @@ TEST(algorithm, sort_ran) {
         EXPECT_EQ(a, b);
     }
 }
+
+namespace LemonUnitTest {
+static const int n = 10000;
+
+struct Negate {
+  typedef int argument_type;
+  typedef int result_type;
+  int operator()(int a) const { return - a; }
+};
+
+int negate(int a) { return - a; }
+
+
+void generateIntSequence(int n, std::vector<int>& data) {
+  int prime = 9973;
+  int root = 136, value = 1;
+  for (int i = 0; i < n; ++i) {
+    data.push_back(value - prime / 2);
+    value = (value * root) % prime;
+  }
+}
+
+void generateCharSequence(int n, std::vector<unsigned char>& data) {
+  int prime = 251;
+  int root = 3, value = root;
+  for (int i = 0; i < n; ++i) {
+    data.push_back(static_cast<unsigned char>(value));
+    value = (value * root) % prime;
+  }
+}
+
+void checkRadixSort() {
+  {
+    std::vector<int> data1;
+    generateIntSequence(n, data1);
+
+    std::vector<int> data2(data1);
+    std::sort(data1.begin(), data1.end());
+
+    radix_sort(data2.begin(), data2.end());
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[i]);
+    }
+
+    radix_sort(data2.begin(), data2.end(), Negate());
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[n - 1 - i]);
+    }
+
+    radix_sort(data2.begin(), data2.end(), negate);
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[n - 1 - i]);
+    }
+
+  }
+
+  {
+    std::vector<unsigned char> data1(n);
+    generateCharSequence(n, data1);
+
+    std::vector<unsigned char> data2(data1);
+    std::sort(data1.begin(), data1.end());
+
+    radix_sort(data2.begin(), data2.end());
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[i]);
+    }
+
+  }
+}
+
+
+void checkStableRadixSort() {
+  {
+    std::vector<int> data1;
+    generateIntSequence(n, data1);
+
+    std::vector<int> data2(data1);
+    std::sort(data1.begin(), data1.end());
+
+    radix_sort(data2.begin(), data2.end());
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[i]);
+    }
+
+    radix_sort(data2.begin(), data2.end(), Negate());
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[n - 1 - i]);
+    }
+
+    radix_sort(data2.begin(), data2.end(), negate);
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[n - 1 - i]);
+    }
+  }
+
+  {
+    std::vector<unsigned char> data1(n);
+    generateCharSequence(n, data1);
+
+    std::vector<unsigned char> data2(data1);
+    std::sort(data1.begin(), data1.end());
+
+    radix_sort(data2.begin(), data2.end());
+    for (int i = 0; i < n; ++i) {
+      EXPECT_EQ(data1[i], data2[i]);
+    }
+
+  }
+}
+
+int main() {
+
+  checkRadixSort();
+  checkStableRadixSort();
+
+  return 0;
+}
+} // namespace LemonUnitTest
+
+TEST(algorithm, radix_sort_lemon) {
+    LemonUnitTest::main();
+}
